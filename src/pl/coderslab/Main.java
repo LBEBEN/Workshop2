677fdb247386c257d53a847ebc217f31d4833cb2
LBEBEN <lukas.beben@gmail.com>
2022-03-11 20:09:33 +0100

    połączenie z bazą danych

--- src/pl/coderslab/Main.java
+++ src/pl/coderslab/Main.java
@@ -1,12 +1,16 @@
 package pl.coderslab;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public class Main {
 
     public static void main(String[] args) {
-	try (Connection connection = DbU){
+        try (Connection connection = DbUtil.ConnectionToWorkshop2()){
 
-    }
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 }
