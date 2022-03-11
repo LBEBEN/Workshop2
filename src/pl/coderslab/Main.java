6bbad16a5179d0ca83efa39ff181430526fb2f25
LBEBEN <lukas.beben@gmail.com>
2022-03-11 19:38:17 +0100

    Startujemy

--- src/pl/coderslab/Main.java
+++ src/pl/coderslab/Main.java
@@ -1,8 +1,12 @@
 package pl.coderslab;
 
+import java.sql.Connection;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	try (Connection connection = DbU){
+
+    }
     }
 }
